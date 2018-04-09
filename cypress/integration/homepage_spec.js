a62d4bbfab409b96b18cb2b7ff0b5b34d87d63e5
Simon Bao <Simonbao.sb@gmail.com>
2018-04-09 19:52:02 +0100

    create mongodb script
    insert issues into database and create indexs

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -1,15 +1,15 @@
-describe('Homepage', function(){
-  
-  it('has loads web application', function(){
+describe('Homepage', function () {
+
+  it('has loads web application', function () {
     cy.visit('localhost:3000')
   })
 
-  it('has Issue from owner "Ravan"', function(){
+  it('has Issue from owner "Ravan"', function () {
     cy.visit('localhost:3000')
     cy.contains('Ravan')
   })
 
-  it('adds issue', function(){
+  it('adds issue', function () {
     cy.visit('localhost:3000')
     cy.get('#issueAdd > [name="owner"]').type('James')
     cy.get('#issueAdd > [name="title"]').type('James Book')
