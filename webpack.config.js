11417720afd9bda052a699696b3c01945ff43c1e
Simon Bao <Simonbao.sb@gmail.com>
2018-04-10 21:02:43 +0100

    Installed webpack/webpack-cli
    used main.bundle.js from webpack in index.html

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack')
+const path = require('path')
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+
+
+
+module.exports = {
+  entry: "./src/App.jsx",
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'static')
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx$/,
+      exclude: /node_modules/,
+
+      use: [{
+        loader: 'babel-loader',
+
+        options: {
+          presets: ['env', 'react', 'es2015']
+        }
+      }]
+    }]
+  },
+
+  plugins: [new UglifyJSPlugin()]
+}
