4b7b2f96af232dfce6238d2d58d93f52ab9a343c
Simon Bao <Simonbao.sb@gmail.com>
2018-04-20 20:42:09 +0100

    Implmentation of switch to load single route dependant component
    Removal of Redirect due to currently unknown bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route, Link, Redirect } from 'react-router-dom';
+import { HashRouter, Route, Link, Redirect, Switch} from 'react-router-dom';
 
 import IssueList from './IssueList.jsx';
 import IssueEdit from './IssueEdit.jsx';
@@ -9,14 +9,16 @@ const contentNode = document.getElementById('contents');
 const noMatch = () => <p> Page Not Found </p>;
 
 const RoutedApp = () => (
-  <HashRouter >
-    <div>
-      <Redirect from="/" to="/issues" />
-      <Route path="/issues" component={IssueList} />
-      <Route path="/IssueEdit" component={IssueEdit} />
-    </div>
-  </HashRouter>
+  <HashRouter>
+    <Switch>
+        <Route exact path="/" component={IssueList} />
+        <Route exact path="/issues" component={IssueList} />
+        <Route exact path="/issues/:id" component={IssueEdit} />
+        <Route exact path="*" component={noMatch} />
+    </Switch>
+</HashRouter>
 )
+
 
 
 ReactDOM.render(<RoutedApp />, contentNode);      
