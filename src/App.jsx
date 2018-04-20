b0083ea21b156d262c76dfc9797370d3f3337807
Simon Bao <Simonbao.sb@gmail.com>
2018-04-20 19:40:44 +0100

    Implementation of hash based routing
    Creation of IssueEdit Component, with placeholder content

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-'use strict';
-import IssueList from './IssueList.jsx'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Route, Link, Redirect } from 'react-router-dom';
+
+import IssueList from './IssueList.jsx';
+import IssueEdit from './IssueEdit.jsx';
 
 const contentNode = document.getElementById('contents');
-ReactDOM.render(<IssueList />, contentNode);      
+const noMatch = () => <p> Page Not Found </p>;
+
+const RoutedApp = () => (
+  <HashRouter >
+    <div>
+      <Redirect from="/" to="/issues" />
+      <Route path="/issues" component={IssueList} />
+      <Route path="/IssueEdit" component={IssueEdit} />
+    </div>
+  </HashRouter>
+)
+
+
+ReactDOM.render(<RoutedApp />, contentNode);      
 // Render the component inside the content Node
---
+++ src/IssueEdit.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class IssueEdit extends React.Component{
+  render(){
+    return(
+      <div>Placeholder for IssueEdit</div>
+    );
+  }
+}
