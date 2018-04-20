798873542dce89fde2bb4dbc76fb03ec0639716a
Simon Bao <Simonbao.sb@gmail.com>
2018-04-20 20:43:18 +0100

    IssueEdit page accepts params id
    IssueEdit page links back to issues page

--- src/IssueEdit.jsx
+++ src/IssueEdit.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export default class IssueEdit extends React.Component{
   render(){
     return(
-      <div>Placeholder for IssueEdit</div>
+      <div>
+        <p>This is a placeholder for editing {this.props.match.params.id}</p>
+        <Link to="/issues"> Back to Issues </Link>
+      </div>
     );
   }
 }
